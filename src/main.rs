799053cc5ad076d3b454d92c3d8d55787644b5de
Niklas Kuhrmeyer <niklas.kuhrmeyer@ptb.de>
2020-10-23 16:54:24 +0200

    added 2nd biquad used in ch1

--- src/main.rs
+++ src/main.rs
@@ -198,6 +198,11 @@ const APP: () = {
         iir_state: [iir::IIRState; 2],
         #[init([iir::IIR { ba: [1., 0., 0., 0., 0.], y_offset: 0., y_min: -SCALE - 1., y_max: SCALE }; 2])]
         iir_ch: [iir::IIR; 2],
+
+        #[init([[0.; 5]; 2])]
+        iir_2_state: [iir::IIRState; 2],
+        #[init([iir::IIR { ba: [0., 0., 0., 0., 0.], y_offset: 0., y_min: -SCALE - 1., y_max: SCALE }; 2])]
+        iir_2_ch: [iir::IIR; 2],
     }
 
     #[init]
@@ -680,15 +685,18 @@ const APP: () = {
         }
     }
 
-    #[task(binds = SPI3, resources = [adc1, dac1, iir_state, iir_ch], priority = 2)]
+    #[task(binds = SPI3, resources = [adc1, dac1, iir_state, iir_ch, iir_2_state, iir_2_ch], priority = 2)]
     fn spi3(c: spi3::Context) {
         c.resources.adc1.spi.ifcr.write(|w| w.eotc().set_bit());
 
         let output: u16 = {
             let a: u16 = c.resources.adc1.read().unwrap();
             let x0 = f32::from(a as i16);
-            let y0 =
-                c.resources.iir_ch[1].update(&mut c.resources.iir_state[1], x0);
+            let y_iir = c.resources.iir_ch[1].update(&mut c.resources.iir_state[1], x0);
+            let y_iir_2 = c.resources.iir_2_ch[1].update(&mut c.resources.iir_2_state[1], x0);
+            let y0 = y_iir + y_iir_2;
+            let y0 = iir::max(-SCALE-1., iir::min(SCALE, y0));
+
             y0 as i16 as u16 ^ 0x8000
         };
 
